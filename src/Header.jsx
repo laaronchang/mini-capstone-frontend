8908943bb97e4c751d237843d3de461566718470
laaronchang <aacidus@gmail.com>
2024-06-03 10:56:33 -0700

    Created src files, edited Header

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+  <div>
+    <header>
+    <a href="#">Home</a> | <a href="#posts-index">All Products</a> | <a href="#products-new">New Product</a>
+  </header>
+  </div>
+  )
+}
